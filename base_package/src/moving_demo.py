909982ca3cb532041a0511aeea313587bfe7f510
Tim McCarthy <timothy.mcc@comcast.net>
2021-11-19 16:29:18 -0500

    spawn and move both models at the same time

--- base_package/src/moving_demo.py
+++ base_package/src/moving_demo.py
@@ -9,43 +9,32 @@ class Move_demo(object):
         rospy.init_node("base_random_move")
         self.rate = rospy.Rate(.5)
 
-        # self.base_publisher = rospy.Publisher('/my_gen3/cmd_vel', Twist, queue_size=10)
-        # self.arm_publisher = rospy.Publisher('/base/cmd_vel', Twist, queue_size=10)
+        self.base_controllers = []
+        self.arm_controllers = []
 
         base_controller_str = "/base/base_{0}_joint_controller/command"
         arm_controller_str = "/my_gen3/kortex_{0}_joint_controller/command"
 
-        controller_str = arm_controller_str
+        self.base_controllers.append(rospy.Publisher(base_controller_str.format('x'), Float64, queue_size=10))
+        self.base_controllers.append(rospy.Publisher(base_controller_str.format('y'), Float64, queue_size=10))
+        self.base_controllers.append(rospy.Publisher(base_controller_str.format('z'), Float64, queue_size=10))
 
-        self.x_publisher = rospy.Publisher(controller_str.format('x'), Float64, queue_size=10)
-        self.y_publisher = rospy.Publisher(controller_str.format('y'), Float64, queue_size=10)
-        self.z_publisher = rospy.Publisher(controller_str.format('z'), Float64, queue_size=10)
+        self.arm_controllers.append(rospy.Publisher(arm_controller_str.format('x'), Float64, queue_size=10))
+        self.arm_controllers.append(rospy.Publisher(arm_controller_str.format('y'), Float64, queue_size=10))
+        self.arm_controllers.append(rospy.Publisher(arm_controller_str.format('z'), Float64, queue_size=10))
 
 
     def back_and_forth(self):
-        vel_msg = Twist()
-
-        vel_msg.linear.x = 0
-        vel_msg.linear.y = 0
-        vel_msg.linear.z = 0
-        vel_msg.angular.x = 0
-        vel_msg.angular.y = 0
-        vel_msg.angular.z = 0
 
         forward = True
 
         while not rospy.is_shutdown():
-            print("publishing to base/cmd_vel")
-            vel_msg.linear.x = 3 if forward else -3
+            print("publishing velocity")
             forward = not forward
-            # self.base_publisher.publish(vel_msg)
-            # self.arm_publisher.publish(vel_msg)
-            self.x_publisher.publish(500 if forward else -500)
-            # time.sleep(1)
-            self.y_publisher.publish(500 if forward else -500)
-            # time.sleep(1)
-            self.z_publisher.publish(500 if forward else -500)
-            # self.rate.sleep()
+
+            self.base_controllers[0].publish(1000 if forward else -1000)
+            self.arm_controllers[0].publish(1000 if forward else -1000)
+
             time.sleep(1)
 
 if __name__ == '__main__':
