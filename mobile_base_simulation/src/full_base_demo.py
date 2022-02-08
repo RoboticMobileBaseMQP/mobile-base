9a6786b8a49b70dcb985df014747402fb26d7c6e
Tim McCarthy <timothy.mcc@comcast.net>
2022-02-07 20:45:03 -0500

    changed arm pos in demo

--- mobile_base_simulation/src/full_base_demo.py
+++ mobile_base_simulation/src/full_base_demo.py
@@ -11,15 +11,13 @@ class FullBaseDemo:
         self.base_controller = BaseControl()
         self.arm_controller = MoveitArmClient()
 
-        # rospy.init_node("manager_node", anonymous=True)
-
         time.sleep(3)
 
 
 if __name__ == "__main__":
     f = FullBaseDemo()
 
-    print("moving base to 2,2")
+    print("moving base ")
     f.base_controller.base_controllers[3].publish(0.0)
     f.base_controller.base_controllers[0].publish(-2.0)
     f.base_controller.base_controllers[1].publish(-1.0)
@@ -32,10 +30,10 @@ if __name__ == "__main__":
 
     pose_goal = geometry_msgs.msg.Pose()
     pose_goal.orientation.w = 1.0
-    pose_goal.position.x = -0.5
-    pose_goal.position.y = -0.5
+    pose_goal.position.x = -3.0
+    pose_goal.position.y = -1.0
     pose_goal.position.z = 0.3
-    print("moving arm to random position")
+    print("moving arm to a position")
     f.arm_controller.move_arm(pose_goal)
 
     time.sleep(6)
