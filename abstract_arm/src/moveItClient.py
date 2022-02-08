4154eb6cfb12470dbfbe818e12bcd441bd933323
ejackson1 <ejackson@wpi.edu>
2022-02-07 21:19:19 -0500

    Fixed local frame issue on client side

--- abstract_arm/src/moveItClient.py
+++ abstract_arm/src/moveItClient.py
@@ -4,53 +4,72 @@ import rospy
 import geometry_msgs.msg
 from abstract_arm.srv import moveToPose
 from std_msgs.msg import Bool
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import tf2_ros
 
+class MoveItClient:
+    def __init__(self) -> None:
+        
+        rospy.init_node("moveit_client", anonymous=True)
 
-def move_arm_client(pose):
-    rospy.wait_for_service('/move_it_planner')
-    try:
-        c = rospy.ServiceProxy('/move_it_planner', moveToPose)
-        success = c(pose)
-        bool = Bool()
-        bool.data = True
-        if success.data == bool:
-            return
-        else:
-            return print(str(success.data.data) + ", unable to find a solution.")
+        self.tfBuffer = tf2_ros.Buffer()
+        listener = tf2_ros.TransformListener(self.tfBuffer)
+        rospy.sleep(1)
+
     
-    except rospy.ServiceException as e:
-        print("Service called failed: %s"%e)
+    def move_arm_client(self, pose):
+        rospy.wait_for_service('/move_it_planner')
+        try:
+            c = rospy.ServiceProxy('/move_it_planner', moveToPose)
+            success = c(pose)
+            bool = Bool()
+            bool.data = True
+            if success.data == bool:
+                return
+            else:
+                return print(str(success.data.data) + ", unable to find a solution.")
+        
+        except rospy.ServiceException as e:
+            print("Service called failed: %s"%e)
 
 if __name__ == "__main__":
+    moveItClient = MoveItClient()
+    
+    try:
+        # Find relationship between wrold and base of robot arm
+        arm_base_name = 'panda_link0' #specific to panda. Need to modularize
+        world2Base = moveItClient.tfBuffer.lookup_transform('world', arm_base_name, rospy.Time())
 
-    # pose_goal = geometry_msgs.msg.Pose()
-    # pose_goal.orientation.w = 1.0
-    # pose_goal.position.x = -0.5
-    # pose_goal.position.y = -0.5
-    # pose_goal.position.z = 0.3
-    # print("Requesting.. ")
-    # move_arm_client(pose_goal)
+        # Random poses inside of the robot arm workspace
+        pose_goal = geometry_msgs.msg.Pose()
+        pose_goal.orientation.w = 0.5
+        pose_goal.position.x = 0.5
+        pose_goal.position.y = -0.5
+        pose_goal.position.z = 0.5
 
-    # pose_goal.orientation.w = 1.0
-    # pose_goal.position.x = 0.4
-    # pose_goal.position.y = -0.2
-    # pose_goal.position.z = 0.4
-    # print("Requesting.. ")
-    # move_arm_client(pose_goal)
+        pose_goal.position.x += world2Base.transform.translation.x
+        pose_goal.position.y += world2Base.transform.translation.y
+        pose_goal.position.z += world2Base.transform.translation.z
+        
+        print("Requesting.. ")
+        print(pose_goal)
+        
+        moveItClient.move_arm_client(pose_goal)
 
-    # pose_goal.orientation.w = 1.0
-    # pose_goal.position.x = -0.4
-    # pose_goal.position.y = -0.2
-    # pose_goal.position.z = 0.4
-    # print("Requesting.. ")
-    # move_arm_client(pose_goal)
+        # pose_goal.orientation.w = 1.0
+        # pose_goal.position.x = 0.4
+        # pose_goal.position.y = -0.2
+        # pose_goal.position.z = 0.4
+        # print("Requesting.. ")
+        # move_arm_client(pose_goal)
 
+        # pose_goal.orientation.w = 1.0
+        # pose_goal.position.x = -0.4
+        # pose_goal.position.y = -0.2
+        # pose_goal.position.z = 0.4
+        # print("Requesting.. ")
+        # move_arm_client(pose_goal)
 
-    pose_goal = geometry_msgs.msg.Pose()
-    pose_goal.orientation.w = 1.0
-    pose_goal.position.x = 0.5
-    pose_goal.position.y = 0.5
-    pose_goal.position.z = -0.3
-    print("Requesting.. ")
-    move_arm_client(pose_goal)
+    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+        rospy.loginfo("TF TRANSFORM ERROR!")
+        pass
+        
--- abstract_arm/src/moveItServer.py
+++ abstract_arm/src/moveItServer.py
@@ -11,26 +11,42 @@ from std_msgs.msg import Bool
 
 class MoveItPlanner:
     def __init__(self) -> None:
-
         rospy.loginfo("Initializing node in namespace " + rospy.get_namespace())
+        joint_state_topic = ['joint_states:=/panda/joint_states'] # specific to panda. Not necessary with Kortex
+        moveit_commander.roscpp_initialize(joint_state_topic)
         
-        moveit_commander.roscpp_initialize(sys.argv)
+        # Initialize nodes and service
         rospy.init_node("move_group_interface", anonymous=True)
-        
         s = rospy.Service('/move_it_planner', moveToPose, self.move_arm)
 
-        print(rospy.get_namespace())
-
-        self.robot = moveit_commander.RobotCommander("robot_description") # 'robot_description
-        self.arm_group_name = "arm"
+        # Initalize MoveIt
+        self.robot = moveit_commander.RobotCommander() # 'robot_description for kortex
+        self.arm_group_name = "panda_arm" # arm for kortex, panda_arm for panda
         self.scene = moveit_commander.PlanningSceneInterface() # ns=rospy.get_namespace()
-        # self.arm_group = moveit_commander.MoveGroupCommander(self.arm_group_name, ns=rospy.get_namespace())
-        # selfdisplay_trajectory_publisher = rospy.Publisher(rospy.get_namespace() + 'move_group/display_planned_path',
-        #                                         moveit_msgs.msg.DisplayTrajectory,
-        #                                         queue_size=20)
+        self.arm_group = moveit_commander.MoveGroupCommander(self.arm_group_name) # ns=rospy.get_namespace()
+        selfdisplay_trajectory_publisher = rospy.Publisher(rospy.get_namespace() + 'move_group/display_planned_path',
+                                                moveit_msgs.msg.DisplayTrajectory,
+                                                queue_size=20)
         
-        self.TOLERANCE = 0.01
-        
+        self.TOLERANCE = 0.01        
+
+        # planning_frame = self.arm_group.get_planning_frame()
+        # print("============ Planning frame: %s" % planning_frame)
+
+        # # We can also print the name of the end-effector link for this group:
+        # eef_link = self.arm_group.get_end_effector_link()
+        # print("============ End effector link: %s" % eef_link)
+
+        # # We can get a list of all the groups in the robot:
+        # group_names = self.robot.get_group_names()
+        # print("============ Available Planning Groups:", self.robot.get_group_names())
+
+        # # Sometimes for debugging it is useful to print the entire state of the
+        # # robot:
+        # print("============ Printing robot state")
+        # print(self.robot.get_current_state())
+        # print("")
+
         rospy.loginfo("Ready to accept poses!")
 
     def get_cartesian_pose(self, arm_group):
