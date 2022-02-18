8ae998f80dd14ea0b7d148080d712929b5d7d0d8
Tim McCarthy <timothy.mcc@comcast.net>
2022-02-18 12:54:14 -0500

    move ed's file to this branch:

---
+++ abstract_arm/src/abstract_arm/mecanum_logic.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs import Joy
+from std_msgs.msg import Float64
+
+
+class MecanumNode:
+    def __init__(self) -> None:
+
+        # Listen to the xbox controller!
+        rospy.Subscriber("/base/teleop_input", Joy, self.sendEfforts)
+
+        # TODO
+        # Listen to reported encoder values
+        rospy.Subscriber("/base/mecanum_encoders", Float64[], self.updatePosition)
+
+        # Publish calculated efforts to low level controllers
+        self.efforts = rospy.Publisher("/base/mecanum_efforts", Float64[], queue_size=10)
+
+        # Publish updates to Simulation
+        base_controller_str = "/base/base_{0}_joint_controller/command"
+        self.xBaseController = rospy.Publisher(base_controller_str.format('x'), Float64, queue_size=10)
+        self.yBaseController = rospy.Publisher(base_controller_str.format('y'), Float64, queue_size=10)
+        self.zBaseController = rospy.Publisher("/base/base_z_rotation_controller/command", Float64, queue_size=10)
+
+
+    def sendEfforts(self, msg):
+        # helper function to send Effort values to cim motors
+        # msg [Joy] 
+        # Code inspired by https://gm0.org/en/latest/docs/software/mecanum-drive.html
+
+        L_JoyX = msg.axes[0]
+        L_JoyY = msg.axes[1] # potentially multiply this value by 1.1 to coutneract imperfect strafing!
+        R__JoyX = msg.axes[2]
+
+        demoninator = max(abs(L_JoyX) + abs(L_JoyY) + abs(R__JoyX), 1)
+
+        # Front of robot
+        # 1 3
+        # 2 4
+        # Rear of Robot
+
+        cim1Effort = Float64((L_JoyY + L_JoyX + R__JoyX) / demoninator * 100)
+        cim2Effort = Float64((L_JoyY - L_JoyX + R__JoyX) / demoninator * 100)
+        cim3Effort = Float64((L_JoyY - L_JoyX - R__JoyX) / demoninator * 100)
+        cim4Effort = Float64(L_JoyY + L_JoyX - R__JoyX) / demoninator * 100)
+
+        self.efforts.publish([cim1Effort, cim2Effort, cim3Effort, cim4Effort])
+
+
+
+
+    def updatePosition(self, msg):
+        # TODO
+        # Publish rough coordinates of x base y base and z rotation to update in sim!
+
+
+
+        return
