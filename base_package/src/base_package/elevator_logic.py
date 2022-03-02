a0003abc503a56b223e7e6e703b858658f6ecb73
Tim McCarthy <timothy.mcc@comcast.net>
2022-03-02 14:41:56 -0500

    added more buttons/functionality to jacks

--- base_package/src/base_package/elevator_logic.py
+++ base_package/src/base_package/elevator_logic.py
@@ -3,7 +3,7 @@
 import rospy
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Float64, Float64MultiArray
-from base_package.msg import effort_list
+from base_package.msg import effort_list, encoder_values
 
 
 class ElevatorNode:
@@ -17,31 +17,39 @@ class ElevatorNode:
 
         # TODO
         # Listen to reported encoder values
-        rospy.Subscriber("/base/elevator_encoders", None, self.updatePosition)
+        rospy.Subscriber("/base/elevator_encoders", encoder_values, self.updatePosition)
+        self.efforts = effort_list()
 
         # Publish calculated efforts to low level controllers
         self.elevator_efforts = rospy.Publisher("/base/elevator_efforts", effort_list, queue_size=10)
 
         # Publish updates to Simulation
-        arm_name = rospy.get_param("arm")
-        arm_namespace = "panda" if arm_name=="panda" else "my_gen3"
-        arm_controller_str = "/" + arm_namespace + "/" + arm_name + "_{0}_joint_controller/command"
+        # arm_name = rospy.get_param("arm")
+        # arm_namespace = "panda" if arm_name=="panda" else "my_gen3"
+        # arm_controller_str = "/" + arm_namespace + "/" + arm_name + "_{0}_joint_controller/command"
 
-        self.zInsertController = rospy.Publisher(arm_controller_str.format('z'), Float64, queue_size=10)
+        # self.zInsertController = rospy.Publisher(arm_controller_str.format('z'), Float64, queue_size=10)
 
     def sendEfforts(self, msg):
         # helper function to send Effort values to cim motors
-        # msg [Joy] 
+        # msg [Joy]
 
-        AB_buttons = 100*(msg.buttons[1] + -msg.buttons[0]) # B up A down
-        XY_buttons = 100*(msg.buttons[3] + -msg.buttons[2]) # Y up X down
-        RL_bumpers = 100*(msg.buttons[5] + -msg.buttons[4]) # RB up LB down
+        speed = 20 if msg.axes[4] < .5 else 100 # right trigger = fast mode
 
-        efforts = effort_list()
-        efforts.Efforts = [AB_buttons, XY_buttons, RL_bumpers]
+        left_jack = speed*(msg.buttons[0] + -msg.buttons[1]) # B up A down
+        back_jack = speed*(msg.buttons[2] + -msg.buttons[3]) # Y up X down
+        right_jack = speed*(msg.buttons[4] + -msg.buttons[5]) # RB up LB down
+
+        if msg.axes[7]:
+            self.efforts.Efforts = [speed*el for el in [msg.axes[7]*3]] # moves all 3 jacks
+        else: 
+            self.efforts.Efforts = [left_jack, back_jack, right_jack] # moves each jack individually
+        
+        # send signal to reset encoders to 0
+        self.efforts.Reset = msg.buttons[7]
 
         # add feedback loop to ensure all motors reach the same height
-        self.elevator_efforts.publish(efforts)
+        self.elevator_efforts.publish(self.efforts)
 
 
     def updatePosition(self, msg):
--- base_package/src/base_package/jack_encoder_readings.py
+++ base_package/src/base_package/jack_encoder_readings.py
@@ -6,7 +6,7 @@ import sys
 import Encoder
 import rospy
 from std_msgs.msg import Int64
-from base_package.msg import encoder_values
+from base_package.msg import effort_list, encoder_values
 # import special message type of a list of 3 int64's
 
 
@@ -29,6 +29,14 @@ class JackEncoderReader:
         self.encL = Encoder.Encoder(JLa, JLb)
         self.encB = Encoder.Encoder(JBa, JBb)
 
+        self.encoder_writer = rospy.Subscriber("/base/elevator_efforts", effort_list, self.check_encoder_reset)
+
+    def check_encoder_reset(self, msg):
+        if msg.Reset:
+            self.encR.write(0)
+            self.encL.write(0)
+            self.encB.write(0)
+    
     def home_config(self):
         # TODO
         # Read pins on limit switches to know if jack is home
