5ed1cae6e1e85131027fb5a68d5afafa2bcdd3c4
Tim McCarthy <timothy.mcc@comcast.net>
2022-04-01 16:24:34 -0400

    switch to polling loop

--- base_package/src/base_package/limit_switches.py
+++ base_package/src/base_package/limit_switches.py
@@ -9,36 +9,44 @@ class LimitSwitches:
 
         if init_node:
             rospy.init_node("limit_switches", anonymous=True)
+        self.rate = rospy.Rate(50) # 50Hz
 
-        self.channels = [23, 22, 21]
+        self.channels = [23, 22, 21] # left, back, right
 
-        self.channel_values = [0,0,0]
+        self.prev_sw = [0,0,0]
         self.reset = jack_reset()
 
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.channels[0], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.channels[0], GPIO.IN, pull_up_down=GPIO.PUD_UP) # TODO: pull up or down?
         GPIO.setup(self.channels[1], GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.channels[2], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-        GPIO.add_event_detect(self.channels[0], GPIO.BOTH, callback=self.event_callback, bouncetime=100)
-        GPIO.add_event_detect(self.channels[1], GPIO.BOTH, callback=self.event_callback, bouncetime=100)
-        GPIO.add_event_detect(self.channels[2], GPIO.BOTH, callback=self.event_callback, bouncetime=100)
+        # GPIO.add_event_detect(self.channels[0], GPIO.BOTH, callback=self.event_callback, bouncetime=100)
+        # GPIO.add_event_detect(self.channels[1], GPIO.BOTH, callback=self.event_callback, bouncetime=100)
+        # GPIO.add_event_detect(self.channels[2], GPIO.BOTH, callback=self.event_callback, bouncetime=100)
 
-        self.switches_publisher = rospy.Publisher("/base/limit_switches", jack_reset, queue_size=10)
+        self.switches_publisher = rospy.Publisher("/base/elevator_resets", jack_reset, queue_size=10)
 
         print("limit switches ready")
-        rospy.spin()
+        while not rospy.is_shutdown(): # polling loop
+            sw = [GPIO.input(channel) for channel in self.channels]
+            if sw != self.prev_sw:
+                self.set_reset_msg(sw)
+                self.switches_publisher.publish(self.reset)
+                self.prev_sw = sw
 
-    def event_callback(self, channel):
-        # print("channel " + str(channel) + " hit")
-        self.channel_values[self.channels.index(channel)] = GPIO.input(channel)
-        self.set_reset_msg()
-        self.switches_publisher.publish(self.reset)
+            self.rate.sleep()
 
-    def set_reset_msg(self):
-        self.reset.reset_left = self.channel_values[0]
-        self.reset.reset_back = self.channel_values[1]
-        self.reset.reset_right = self.channel_values[2]
+    # def event_callback(self, channel):
+    #     # print("channel " + str(channel) + " hit")
+    #     self.channel_values[self.channels.index(channel)] = GPIO.input(channel)
+    #     self.set_reset_msg()
+    #     self.switches_publisher.publish(self.reset)
+
+    def set_reset_msg(self, sw):
+        self.reset.reset_left = sw[0]
+        self.reset.reset_back = sw[1]
+        self.reset.reset_right = sw[2]
 
 if __name__ == "__main__":
     l = LimitSwitches(init_node=True)
